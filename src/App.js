d5addae5652d007e305694f9316e5feaf52fcccb
Yiseul Kim <yiseulkim@Yiseului-MacBookPro.local>
2023-08-28 18:03:27 +0900

    feat:axios연결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
+import axios from "axios"; // 설치 후 import
 
 class App extends Component {
+  state = {
+    loading: false,
+    ItemList: [],
+  };
+  loadItem = async () => {
+    axios
+      .get("./SearchJson.json")
+      .then(({ data }) => {
+        this.setState({
+          loading: true,
+          ItemList: data.Item,
+        });
+      })
+      .catch((e) => {
+        // API 호출이 실패한 경우
+        console.error(e); // 에러표시
+        this.setState({
+          loading: false,
+        });
+      });
+  };
+
   render() {
     return (
       <div className="App">
